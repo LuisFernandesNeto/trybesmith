d355b0f6bfabcce413fb4eefbad3ef1b9eb99473
Luis Fernandes Neto <luisfernandesneto@gmail.com>
2022-12-14 15:44:59 -0300

    Requisito 1

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import ProductsController from './controllers/products.controller';
 
 const app = express();
 
 app.use(express.json());
+
+const productsController = new ProductsController();
+
+app.post('/products', productsController.create);
 
 export default app;
---
+++ src/services/products.service.ts
@@ -0,0 +1,18 @@
+import connection from "../models/connection";
+import ProductModel from "../models/products.model";
+import Product from "../interfaces/products.interface";
+
+class ProductService {
+    public model: ProductModel;
+
+  constructor() {
+    this.model = new ProductModel(connection);
+  } 
+
+  public create(product: Product): Promise<Product> {
+    
+    return this.model.create(product);
+    }
+}
+
+export default ProductService;
